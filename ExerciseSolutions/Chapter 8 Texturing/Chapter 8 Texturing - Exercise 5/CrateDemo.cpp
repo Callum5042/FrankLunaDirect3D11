4ee2f05b3ef08215b9786ef7fa7c2711b3b5d766
Callum Anning <Callum5042@gmail.com>
2023-07-26 20:11:04 +0100

    Exercise 5

---
+++ ExerciseSolutions/Chapter 8 Texturing/Chapter 8 Texturing - Exercise 5/CrateDemo.cpp
@@ -0,0 +1,356 @@
+//***************************************************************************************
+// CrateDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates texturing a box.
+//
+// Controls:
+//		Hold the left mouse button down and move the mouse to rotate.
+//      Hold the right mouse button down to zoom in and out.
+// 
+// Exercise 5:
+//		This chapter's downloadable directory contains a folder with 120 frames
+//		of a fire animation designed to be played over 4 seconds (30 frames per
+//		second). Figure 8.21 shows teh first 30 frames. Modify the "Crate" demo by
+//		playing this animation over each face of the cube. (Hint: Load the images
+//		into an array of 120 texture objects. Start by using the first frame texture,
+//		and every 1/30th of a second, increment to the next frame texture. After the
+//		120th frame texture, roll back to the first texture and repeat the process.)
+//		This is sometimes called page filipping, because it is reminiscent of flipping
+//		the pages in a flip book. Note: Working with individual texture animation
+//		one-by-one like this is inefficient. It would be better to pull all the
+//		frames in one texture atlas, and then offset the texture coordinates every
+//		1/30th of a second to the next frame of the animation. But for the sake of
+//		the exercise, the inefficient method is fine.
+//
+//***************************************************************************************
+
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+#include "LightHelper.h"
+#include "Effects.h"
+#include "Vertex.h"
+#include <array>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "WICTextureLoader.h"
+
+class CrateApp : public D3DApp
+{
+public:
+	CrateApp(HINSTANCE hInstance);
+	~CrateApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene(); 
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	void BuildGeometryBuffers();
+
+private:
+	ID3D11Buffer* mBoxVB;
+	ID3D11Buffer* mBoxIB;
+
+	std::array<ID3D11ShaderResourceView*, 120> mDiffuseMapSRVs;
+	int mFrameIndex = 0;
+
+	DirectionalLight mDirLights[3];
+	Material mBoxMat;
+
+	XMFLOAT4X4 mTexTransform;
+	XMFLOAT4X4 mBoxWorld;
+
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	int mBoxVertexOffset;
+	UINT mBoxIndexOffset;
+	UINT mBoxIndexCount;
+
+	XMFLOAT3 mEyePosW;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+	CrateApp theApp(hInstance);
+	
+	if( !theApp.Init() )
+		return 0;
+	
+	return theApp.Run();
+}
+ 
+
+CrateApp::CrateApp(HINSTANCE hInstance)
+: D3DApp(hInstance), mBoxVB(0), mBoxIB(0), mEyePosW(0.0f, 0.0f, 0.0f),
+  mTheta(1.3f*MathHelper::Pi), mPhi(0.4f*MathHelper::Pi), mRadius(2.5f)
+{
+	mMainWndCaption = L"Chapter 8 Texturing - Exercise 5";
+	
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mBoxWorld, I);
+	XMStoreFloat4x4(&mTexTransform, I);
+	XMStoreFloat4x4(&mView, I);
+	XMStoreFloat4x4(&mProj, I);
+
+	mDirLights[0].Ambient  = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	mDirLights[0].Diffuse  = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
+	mDirLights[0].Specular = XMFLOAT4(0.6f, 0.6f, 0.6f, 16.0f);
+	mDirLights[0].Direction = XMFLOAT3(0.707f, -0.707f, 0.0f);
+ 
+	mDirLights[1].Ambient  = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+	mDirLights[1].Diffuse  = XMFLOAT4(1.4f, 1.4f, 1.4f, 1.0f);
+	mDirLights[1].Specular = XMFLOAT4(0.3f, 0.3f, 0.3f, 16.0f);
+	mDirLights[1].Direction = XMFLOAT3(-0.707f, 0.0f, 0.707f);
+
+	mBoxMat.Ambient  = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
+	mBoxMat.Diffuse  = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	mBoxMat.Specular = XMFLOAT4(0.6f, 0.6f, 0.6f, 16.0f);
+}
+
+CrateApp::~CrateApp()
+{
+	ReleaseCOM(mBoxVB);
+	ReleaseCOM(mBoxIB);
+
+	for (auto& texture : mDiffuseMapSRVs)
+	{
+		ReleaseCOM(texture);
+	}
+
+	Effects::DestroyAll();
+	InputLayouts::DestroyAll();
+}
+
+bool CrateApp::Init()
+{
+	if(!D3DApp::Init())
+		return false;
+
+	// Must init Effects first since InputLayouts depend on shader signatures.
+	Effects::InitAll(md3dDevice);
+	InputLayouts::InitAll(md3dDevice);
+
+	ID3D11Resource* texResource = nullptr;
+	for (int i = 0; i < 120; ++i)
+	{
+		std::wstringstream ss;
+		ss << L"Textures/FireAnim/Fire" << std::setw(3) << std::setfill(L'0') << (i + 1) << L".bmp";
+
+		HR(DirectX::CreateWICTextureFromFile(md3dDevice, ss.str().c_str(), &texResource, &mDiffuseMapSRVs[i]));
+	}
+
+	texResource->Release();
+ 
+	BuildGeometryBuffers();
+
+	return true;
+}
+
+void CrateApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
+}
+
+void CrateApp::UpdateScene(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = mRadius*sinf(mPhi)*cosf(mTheta);
+	float z = mRadius*sinf(mPhi)*sinf(mTheta);
+	float y = mRadius*cosf(mPhi);
+
+	mEyePosW = XMFLOAT3(x, y, z);
+
+	// Build the view matrix.
+	XMVECTOR pos    = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up     = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&mView, V);
+}
+
+void CrateApp::DrawScene()
+{
+	// Update texture animation index
+	mFrameIndex = (static_cast<int>(mTimer.TotalTime() * 30) % 120);
+
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	md3dImmediateContext->IASetInputLayout(InputLayouts::Basic32);
+    md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+ 
+	UINT stride = sizeof(Vertex::Basic32);
+    UINT offset = 0;
+ 
+	XMMATRIX view  = XMLoadFloat4x4(&mView);
+	XMMATRIX proj  = XMLoadFloat4x4(&mProj);
+	XMMATRIX viewProj = view*proj;
+
+	// Set per frame constants.
+	Effects::BasicFX->SetDirLights(mDirLights);
+	Effects::BasicFX->SetEyePosW(mEyePosW);
+ 
+	ID3DX11EffectTechnique* activeTech = Effects::BasicFX->Light2TexTech;
+
+    D3DX11_TECHNIQUE_DESC techDesc;
+	activeTech->GetDesc( &techDesc );
+    for(UINT p = 0; p < techDesc.Passes; ++p)
+    {
+		md3dImmediateContext->IASetVertexBuffers(0, 1, &mBoxVB, &stride, &offset);
+		md3dImmediateContext->IASetIndexBuffer(mBoxIB, DXGI_FORMAT_R32_UINT, 0);
+
+		// Draw the box.
+		XMMATRIX world = XMLoadFloat4x4(&mBoxWorld);
+		XMMATRIX worldInvTranspose = MathHelper::InverseTranspose(world);
+		XMMATRIX worldViewProj = world*view*proj;
+
+		Effects::BasicFX->SetWorld(world);
+		Effects::BasicFX->SetWorldInvTranspose(worldInvTranspose);
+		Effects::BasicFX->SetWorldViewProj(worldViewProj);
+		Effects::BasicFX->SetTexTransform(XMLoadFloat4x4(&mTexTransform));
+		Effects::BasicFX->SetMaterial(mBoxMat);
+		Effects::BasicFX->SetDiffuseMap(mDiffuseMapSRVs[mFrameIndex]);
+
+		activeTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+		md3dImmediateContext->DrawIndexed(mBoxIndexCount, mBoxIndexOffset, mBoxVertexOffset);
+    }
+
+	HR(mSwapChain->Present(0, 0));
+}
+
+void CrateApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void CrateApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void CrateApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if( (btnState & MK_LBUTTON) != 0 )
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi   += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi-0.1f);
+	}
+	else if( (btnState & MK_RBUTTON) != 0 )
+	{
+		// Make each pixel correspond to 0.01 unit in the scene.
+		float dx = 0.01f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.01f*static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 1.0f, 15.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void CrateApp::BuildGeometryBuffers()
+{
+	GeometryGenerator::MeshData box;
+
+	GeometryGenerator geoGen;
+	geoGen.CreateBox(1.0f, 1.0f, 1.0f, box);
+
+	// Cache the vertex offsets to each object in the concatenated vertex buffer.
+	mBoxVertexOffset      = 0;
+
+	// Cache the index count of each object.
+	mBoxIndexCount      = box.Indices.size();
+
+	// Cache the starting index for each object in the concatenated index buffer.
+	mBoxIndexOffset      = 0;
+	
+	UINT totalVertexCount = box.Vertices.size();
+
+	UINT totalIndexCount = mBoxIndexCount;
+
+	//
+	// Extract the vertex elements we are interested in and pack the
+	// vertices of all the meshes into one vertex buffer.
+	//
+
+	std::vector<Vertex::Basic32> vertices(totalVertexCount);
+
+	UINT k = 0;
+	for(size_t i = 0; i < box.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos    = box.Vertices[i].Position;
+		vertices[k].Normal = box.Vertices[i].Normal;
+		vertices[k].Tex    = box.Vertices[i].TexC;
+	}
+
+    D3D11_BUFFER_DESC vbd;
+    vbd.Usage = D3D11_USAGE_IMMUTABLE;
+    vbd.ByteWidth = sizeof(Vertex::Basic32) * totalVertexCount;
+    vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    vbd.CPUAccessFlags = 0;
+    vbd.MiscFlags = 0;
+    D3D11_SUBRESOURCE_DATA vinitData;
+    vinitData.pSysMem = &vertices[0];
+    HR(md3dDevice->CreateBuffer(&vbd, &vinitData, &mBoxVB));
+
+	//
+	// Pack the indices of all the meshes into one index buffer.
+	//
+
+	std::vector<UINT> indices;
+	indices.insert(indices.end(), box.Indices.begin(), box.Indices.end());
+
+	D3D11_BUFFER_DESC ibd;
+    ibd.Usage = D3D11_USAGE_IMMUTABLE;
+    ibd.ByteWidth = sizeof(UINT) * totalIndexCount;
+    ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    ibd.CPUAccessFlags = 0;
+    ibd.MiscFlags = 0;
+    D3D11_SUBRESOURCE_DATA iinitData;
+    iinitData.pSysMem = &indices[0];
+    HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mBoxIB));
+}
+ 
