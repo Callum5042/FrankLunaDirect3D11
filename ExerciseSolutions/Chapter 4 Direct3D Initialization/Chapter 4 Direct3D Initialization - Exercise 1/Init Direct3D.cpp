29516350501b91029b3bfd8434e75a76880b069e
Callum Anning <Callum5042@gmail.com>
2023-07-27 21:08:15 +0100

    Exercise 1

---
+++ ExerciseSolutions/Chapter 4 Direct3D Initialization/Chapter 4 Direct3D Initialization - Exercise 1/Init Direct3D.cpp
@@ -0,0 +1,91 @@
+//***************************************************************************************
+// Init Direct3D.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates the sample framework by initializing Direct3D, clearing 
+// the screen, and displaying frame stats.
+//
+//***************************************************************************************
+
+#include "d3dApp.h"
+ 
+class InitDirect3DApp : public D3DApp
+{
+public:
+	InitDirect3DApp(HINSTANCE hInstance);
+	~InitDirect3DApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene(); 
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+	InitDirect3DApp theApp(hInstance);
+	
+	if( !theApp.Init() )
+		return 0;
+	
+	return theApp.Run();
+}
+
+InitDirect3DApp::InitDirect3DApp(HINSTANCE hInstance)
+: D3DApp(hInstance) 
+{
+}
+
+InitDirect3DApp::~InitDirect3DApp()
+{
+}
+
+bool InitDirect3DApp::Init()
+{
+	if(!D3DApp::Init())
+		return false;
+
+	// Disable ALT+ENTER
+	IDXGIDevice* dxgiDevice = 0;
+	HR(md3dDevice->QueryInterface(__uuidof(IDXGIDevice), (void**)&dxgiDevice));
+
+	IDXGIAdapter* dxgiAdapter = 0;
+	HR(dxgiDevice->GetParent(__uuidof(IDXGIAdapter), (void**)&dxgiAdapter));
+
+	IDXGIFactory* dxgiFactory = 0;
+	HR(dxgiAdapter->GetParent(__uuidof(IDXGIFactory), (void**)&dxgiFactory));
+
+	dxgiFactory->MakeWindowAssociation(mhMainWnd, DXGI_MWA_NO_ALT_ENTER);
+
+	ReleaseCOM(dxgiDevice);
+	ReleaseCOM(dxgiAdapter);
+	ReleaseCOM(dxgiFactory);
+
+	return true;
+}
+
+void InitDirect3DApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void InitDirect3DApp::UpdateScene(float dt)
+{
+
+}
+
+void InitDirect3DApp::DrawScene()
+{
+	assert(md3dImmediateContext);
+	assert(mSwapChain);
+
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::Blue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	HR(mSwapChain->Present(0, 0));
+}
